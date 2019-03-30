3888f39221016d963a6a0f63351f0d5576c0dde4
aussftw <shagronaus@gmail.com>
2019-03-30 20:39:14 +0200

    coords api_call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-
-import axios from "axios";
-
 import Header from "./Header";
 import Form from "./Form";
 import Result from "./Result";
+import Footer from "./Footer/Footer";
 import "./App.css";
 
 class App extends Component {
@@ -15,33 +13,60 @@ class App extends Component {
       actualWeather: {
         temp: null,
         wind: null,
-        press: null
+        press: null,
+        humidity: null
       },
       windchill: null,
-      visible: false
+      visible: false,
+      forecast: []
     };
   }
 
-  getWeather = e => {
+  getWeather = async e => {
     e.preventDefault();
     const API_KEY = "d4502d305d879beb12e6f13ebb40722d";
     const city = e.target.elements.city.value;
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pl&cnt=5&appid=${API_KEY}`
-      )
-      .then(data => {
-        console.log(data);
-        this.setState({
-          city: data.data.name,
-          actualWeather: {
-            temp: data.data.main.temp,
-            wind: data.data.wind.speed,
-            press: data.data.main.pressure
-          },
-          visible: true
-        });
-      });
+    const temp = this.state.actualWeather.temp;
+    const wind = this.state.actualWeather.wind;
+
+    const apiСallWeather = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+    );
+
+    const response = await apiСallWeather.json();
+
+    this.setState({
+      city: response.name,
+      actualWeather: {
+        temp: response.main.temp,
+        wind: response.wind.speed,
+        press: response.main.pressure,
+        humidity: response.main.humidity
+      },
+      visible: true
+    });
+    const apiCallForecast = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`
+    );
+    const forecastResponse = await apiCallForecast.json();
+
+    this.setState({ forecast: forecastResponse.list });
+
+    //const apiCallCoords = await fetch(
+    // `https://api.openweathermap.org/data/2.5/weather?q=lat=35&lon=139&units=metric&appid=${API_KEY}`
+    //);
+    //const responseCallCoords = await apiCallCoords.json();
+    //console.log(responseCallCoords);
+
+    const windChillCalc = Math.round(
+      15.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(wind, 0.16) +
+        0.3965 * temp * Math.pow(wind, 0, 16)
+    );
+    this.setState({ windchill: windChillCalc });
+
+    //  console.log(this.state);
   };
 
   render() {
@@ -49,7 +74,12 @@ class App extends Component {
       <div className="container">
         <Header />
         <Form loadWeather={this.getWeather} />
-        <Result />
+        <Result
+          city={this.state.city}
+          item={this.state.actualWeather}
+          windchill={this.state.windchill}
+          forecast={this.state.forecast}
+        />
       </div>
     );
   }
--- src/Result.js
+++ src/Result.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Result = props => {
-  return <div>Weather Result</div>;
+  return (
+    <div>
+      <h1>{props.city}</h1>
+      <h3>{props.item.temp}</h3>
+      <p>{props.item.wind}</p>
+      <h3>{props.windchill}</h3>
+      <p>pressure {props.item.press}</p>
+      <p>humidity {props.item.humidity}</p>
+    </div>
+  );
 };
 
 export default Result;
