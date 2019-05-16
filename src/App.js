90a5cec4f98995b8bff928169d50e8932d5d2e96
aussftw <shagronaus@gmail.com>
2019-05-16 15:25:32 +0300

    coords button moved to form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from "react";
-import Form from "./Components/Form/Form";
-import Result from "./Components/Result/Result";
-import Footer from "./Components/Footer/Footer";
+import React, { Component } from "react"
+import Form from "./Components/Form/Form"
+import Result from "./Components/Result/Result"
+import Footer from "./Components/Footer/Footer"
 
-import { DEFAULT_WEATHER_URL, DEFAULT_UNSPLASH_URL, API_KEY_WEATHER, API_KEY_UNSPLASH } from "./consts";
-import "./style.scss";
+import {
+  DEFAULT_WEATHER_URL,
+  DEFAULT_UNSPLASH_URL,
+  API_KEY_WEATHER,
+  API_KEY_UNSPLASH
+} from "./consts"
+import "./style.scss"
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       city: "",
       lat: null,
@@ -28,7 +33,7 @@ class App extends Component {
       cityBackground: {
         img: ""
       }
-    };
+    }
   }
 
   componentDidMount() {
@@ -39,17 +44,19 @@ class App extends Component {
           long: position.coords.longitude
         }),
       err => this.setState({ errorMessage: err.message })
-    );
+    )
   }
 
   getInputWeather = async e => {
-    e.preventDefault();
-    const city = e.target.elements.city.value;
-    const temp = Math.round(this.state.actualWeather.temp);
-    const wind = Math.round(this.state.actualWeather.wind);
+    e.preventDefault()
+    const city = e.target.elements.city.value
+    const temp = Math.round(this.state.actualWeather.temp)
+    const wind = Math.round(this.state.actualWeather.wind)
 
-    const apiСallWeather = await fetch(`${DEFAULT_WEATHER_URL}weather?q=${city}&units=metric&appid=${API_KEY_WEATHER}`);
-    const response = await apiСallWeather.json();
+    const apiСallWeather = await fetch(
+      `${DEFAULT_WEATHER_URL}weather?q=${city}&units=metric&appid=${API_KEY_WEATHER}`
+    )
+    const response = await apiСallWeather.json()
 
     this.setState({
       city: response.name,
@@ -61,45 +68,52 @@ class App extends Component {
         desc: response.weather[0].description
       },
       visible: true
-    });
-    const apiCallForecast = await fetch(`${DEFAULT_WEATHER_URL}forecast?q=${city}&units=metric&appid=${API_KEY_WEATHER}`);
-    const forecastResponse = await apiCallForecast.json();
-    const oldForecatstList = forecastResponse.list;
-    const required = [7, 15, 23, 31, 39];
-    const forecastList = [];
-    const oldForecatstListLength = oldForecatstList.length;
+    })
+    const apiCallForecast = await fetch(
+      `${DEFAULT_WEATHER_URL}forecast?q=${city}&units=metric&appid=${API_KEY_WEATHER}`
+    )
+    const forecastResponse = await apiCallForecast.json()
+    const oldForecatstList = forecastResponse.list
+    const required = [7, 15, 23, 31, 39]
+    const forecastList = []
+    const oldForecatstListLength = oldForecatstList.length
     for (var i = 0; i < oldForecatstListLength; i++) {
       if (required.includes(i + 1)) {
-        forecastList.push(oldForecatstList[i]);
+        forecastList.push(oldForecatstList[i])
       }
     }
-    this.setState({ forecast: { list: forecastList } });
+    this.setState({ forecast: { list: forecastList } })
 
-    const unsplashCall = await fetch(`${DEFAULT_UNSPLASH_URL}?page=1&per_page=10&query=${city}&client_id=${API_KEY_UNSPLASH}`);
-    const unsplashResponse = await unsplashCall.json();
+    const unsplashCall = await fetch(
+      `${DEFAULT_UNSPLASH_URL}?page=1&per_page=10&query=${city}&client_id=${API_KEY_UNSPLASH}`
+    )
+    const unsplashResponse = await unsplashCall.json()
     this.setState({
       cityBackground: {
         img: unsplashResponse.results[0].urls.regular
       }
-    });
+    })
 
     const windChillCalc = Math.round(
-      15.12 + 0.6215 * temp - 11.37 * Math.pow(wind, 0.16) + 0.3965 * temp * Math.pow(wind, 0, 16)
-    );
-    this.setState({ windchill: windChillCalc });
-    console.log(this.state);
-  };
+      15.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(wind, 0.16) +
+        0.3965 * temp * Math.pow(wind, 0, 16)
+    )
+    this.setState({ windchill: windChillCalc })
+    console.log(this.state)
+  }
 
   getCoordsWeather = async e => {
-    e.preventDefault();
+    const lat = this.state.lat
+    const long = this.state.long
+    const temp = Math.round(this.state.actualWeather.temp)
+    const wind = Math.round(this.state.actualWeather.wind)
 
-    const lat = this.state.lat;
-    const long = this.state.long;
-    const temp = Math.round(this.state.actualWeather.temp);
-    const wind = Math.round(this.state.actualWeather.wind);
-
-    const apiCall = await fetch(`${DEFAULT_WEATHER_URL}weather?lat=${lat}&lon=${long}&units=metric&appid=${API_KEY_WEATHER}`);
-    const response = await apiCall.json();
+    const apiCall = await fetch(
+      `${DEFAULT_WEATHER_URL}weather?lat=${lat}&lon=${long}&units=metric&appid=${API_KEY_WEATHER}`
+    )
+    const response = await apiCall.json()
     this.setState({
       city: response.name,
       actualWeather: {
@@ -110,57 +124,56 @@ class App extends Component {
         desc: response.weather[0].description
       },
       visible: true
-    });
+    })
 
     const apiCallForecast = await fetch(
       `${DEFAULT_WEATHER_URL}forecast?lat=${lat}&lon=${long}&units=metric&appid=${API_KEY_WEATHER}`
-    );
-    const forecastResponse = await apiCallForecast.json();
+    )
+    const forecastResponse = await apiCallForecast.json()
 
-    const oldForecatstList = forecastResponse.list;
-    const required = [7, 15, 23, 31, 39];
-    const forecastList = [];
-    const oldForecatstListLength = oldForecatstList.length;
+    const oldForecatstList = forecastResponse.list
+    const required = [7, 15, 23, 31, 39]
+    const forecastList = []
+    const oldForecatstListLength = oldForecatstList.length
     for (var i = 0; i < oldForecatstListLength; i++) {
       if (required.includes(i + 1)) {
-        forecastList.push(oldForecatstList[i]);
+        forecastList.push(oldForecatstList[i])
       }
     }
-    this.setState({ forecast: { list: forecastList } });
+    this.setState({ forecast: { list: forecastList } })
 
     const unsplashCall = await fetch(
-      `${DEFAULT_UNSPLASH_URL}/?page=1&per_page=10&query=${this.state.city}&client_id=${API_KEY_UNSPLASH}`
-    );
-    const unsplashResponse = await unsplashCall.json();
+      `${DEFAULT_UNSPLASH_URL}/?page=1&per_page=10&query=${
+        this.state.city
+      }&client_id=${API_KEY_UNSPLASH}`
+    )
+    const unsplashResponse = await unsplashCall.json()
     this.setState({
       cityBackground: {
         img: unsplashResponse.results[0].urls.regular
       }
-    });
+    })
 
     const windChillCalc = Math.round(
-      15.12 + 0.6215 * temp - 11.37 * Math.pow(wind, 0.16) + 0.3965 * temp * Math.pow(wind, 0, 16)
-    );
-    this.setState({ windchill: windChillCalc });
+      15.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(wind, 0.16) +
+        0.3965 * temp * Math.pow(wind, 0, 16)
+    )
+    this.setState({ windchill: windChillCalc })
 
-    console.log(this.state);
-  };
-
-  renderButton() {
-    return (
-      <div className={`coords-button ${this.state.visible === true ? "toggler" : ""}`} onClick={this.getCoordsWeather}>
-        {" "}
-        <i className="large location arrow icon" />{" "}
-      </div>
-    );
+    console.log(this.state)
   }
 
   render() {
     return (
       <div>
         <div>
-          <Form loadInputWeather={this.getInputWeather} visible={this.state.visible} />
-          <div>{this.renderButton()}</div>
+          <Form
+            loadInputWeather={this.getInputWeather}
+            visible={this.state.visible}
+            coordsButton={this.getCoordsWeather}
+          />
         </div>
         <div className={` ${this.state.visible === true ? "" : "hidden"}`}>
           <Result
@@ -173,8 +186,8 @@ class App extends Component {
           {/* <Footer /> */}
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 // import "./Form.css"
 
 const Form = props => {
@@ -7,14 +7,23 @@ const Form = props => {
       <div className="search-form-wrapper">
         <p className="p-header">Look at weather at any city</p>
         <form onSubmit={props.loadInputWeather} className="search-form">
-          <input type="text" name="city" placeholder="Enter a city" className="input-data" required />
+          <input
+            type="text"
+            name="city"
+            placeholder="Enter a city"
+            className="input-data"
+            required
+          />
           <button className="input-button">
             <i className="large search icon" />
+          </button>
+          <button className="input-button" onClick={() => props.coordsButton()}>
+            <i className="large location arrow icon" />
           </button>
         </form>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Form;
+export default Form
